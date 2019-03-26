19f9d22c0889edb70383fc21765588bba1b73c9c
Guilherme <guilherme.sobu@outlook.com>
2019-03-26 17:49:25 -0300

    Consertando div do Main e Side content

--- museu/index.php
+++ museu/index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html>
+    
     <head>
         <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"> 
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <title>MUSEUFODAO!!</title>
     </head>
+    
     <body>
+        
         <div>
             <?php
                 require 'pages/header.html';
@@ -13,28 +16,25 @@
             ?>
         </div>
         
-        <table>
-            <tbody>
-                <tr class="index-div-content">
-                    <td class="main-div">
-                        <?php 
-                            require './pages/maincontent.html'; 
-                        ?>
-                    </td>
-                    <td class="side-div">
-                        <?php 
-                            require './pages/sidecontent.html';
-                        ?>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="index-div-content">
+            <div class="main-div">
+                <?php 
+                    require './pages/maincontent.html'; 
+                ?>
+            </div>
+            <div class="side-div">
+                <?php 
+                    require './pages/sidecontent.html';
+                ?>
+            </div>
+        </div>
         
-        <div class="index-div-footer">
+        <div class="index-div-footer footer-div">
             <?php 
                 require './pages/footer.html';
             ?>
         </div>
+        
     </body>
 </html>
 
