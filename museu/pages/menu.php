ac6f409a38ac98596d20e3872f7344e10523ef22
Guilherme <guilherme.sobu@outlook.com>
2019-03-27 06:16:58 -0300

    Tentativa de alteração das tags A

--- museu/pages/menu.php
+++ museu/pages/menu.php
@@ -1,5 +1,7 @@
-<div class="menu-block menu-font">
-    Pesquise! <input type="text" name="query">
-    <a class="menu-href" href="index.php">Home</a>
-    <a class="menu-href" href="http://www.google.com.br">Google</a>
+<div class="menu-block">
+    <div class="menu-font">
+        <a class="menu-href" href="index.php">Home</a>
+        <a class="menu-href" href="http://www.google.com.br">Google</a>
+        <a class="menu-href">Pesquise! <input type="text" name="query"></a>
+    </div>    
 </div>
