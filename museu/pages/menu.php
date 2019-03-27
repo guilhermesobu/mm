592133b7f8600f3ef6bb6dff7c238371d61b7d40
Guilherme <guilherme.sobu@outlook.com>
2019-03-27 05:51:07 -0300

    Começando com pesquisa

--- museu/pages/menu.php
+++ museu/pages/menu.php
@@ -1,4 +1,5 @@
 <div class="menu-block menu-font">
+    Pesquise! <input type="text" name="query">
     <a class="menu-href" href="index.php">Home</a>
     <a class="menu-href" href="http://www.google.com.br">Google</a>
 </div>
