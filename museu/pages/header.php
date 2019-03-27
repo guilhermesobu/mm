16ce29881683ac1b57727c5d7a70feeb971728e0
Guilherme <guilherme.sobu@outlook.com>
2019-03-27 06:57:02 -0300

    Inserindo botão de pesquisa PHP

--- museu/pages/header.php
+++ museu/pages/header.php
@@ -1,4 +1,11 @@
 <div class="header-block">
+    <form action="../index.php" method="post">    
+        <a class="menu-href header-position">
+            Pesquise! 
+            <input type="text" name="query">
+            <input type="submit" value="Vai!">
+        </a>
+    </form>    
     <p class="header-font">
         Museum
     </p>
--- museu/pages/menu.php
+++ museu/pages/menu.php
@@ -2,6 +2,5 @@
     <div class="menu-font">
         <a class="menu-href" href="index.php">Home</a>
         <a class="menu-href" href="http://www.google.com.br">Google</a>
-        <a class="menu-href">Pesquise! <input type="text" name="query"></a>
     </div>    
 </div>
