7dc8612a2b1b3a251a8dc77e14a1658bf6229a86
guilhermesobu <guilherme.sobu@outlook.com>
2019-03-28 18:14:22 -0300

    começando de fato com php

--- museu/pages/header.php
+++ museu/pages/header.php
@@ -1,12 +1,13 @@
 <div class="header-block">
-    <form action="index.php" method="get">    
-        <a class="menu-query header-position">
+    <form action="index.php" method="get" 
+          class="header-query header-position header-padding">    
+        <a>
             Pesquise! 
             <input type="text" name="query">
             <input type="submit" value="Vai!">
         </a>
     </form>    
-    <a class="header-font">
+    <a class="header-font header-padding">
         Museum
     </a>
 </div>
