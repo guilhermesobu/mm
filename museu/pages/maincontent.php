fd38bbb1972bab76867983b6ffe27c3f0a9d7d63
Guilherme <guilherme.sobu@outlook.com>
2019-04-02 08:06:04 -0300

    Array conesrtado pelo Simeoni

--- museu/pages/maincontent.php
+++ museu/pages/maincontent.php
@@ -9,9 +9,9 @@
             $results = mysqli_query($connection, $select_all);
             var_dump($results);
             echo "<br><br>";
-            foreach($results as $key => $info){
-                print_r($info);
-                echo "<br>";
+            foreach($results as $info){
+                //print_r($info);
+                echo $info['name']."<br>";
             }
         }
     ?>
