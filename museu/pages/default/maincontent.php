e1ede7bebedc66a839d70c69330017d5c97bb15a
guilhermesobu <guilherme.sobu@outlook.com>
2019-04-06 18:57:32 -0300

    Adicionando ordem nas informaçẽs retornadas

--- museu/pages/default/maincontent.php
+++ museu/pages/default/maincontent.php
@@ -1,22 +1,41 @@
 <div class="main-p">
-    <?php 
-        require 'src/query/connection.php';
-        require 'src/query/query.php';
-        if (isset($_POST['query'])){
-            $query = $_POST['query'];
-            $results = mysqli_query($connection, search($query));
-            foreach ($results as $info){    
-                echo "Nome: " . $info['name'] . "<br>";
-                echo "Descrição: " . $info['description'] . "<br>";
-                echo "Departamento: " . $info['department'] . "<br><br>";
-            }
-        } else {
-            $results = mysqli_query($connection, $select_all);
-            foreach ($results as $info){
-                echo "Nome: " . $info['name'] . "<br>";
-                echo "Descrição: " . $info['description'] . "<br>";
-                echo "Departamento: " . $info['department'] . "<br><br>";
-            }
-        }
-    ?>
+    <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Descrição</th>
+            <th>Departamento</th>
+          </tr>
+        </thead>
+        <tbody>
+            <?php 
+                require 'src/query/connection.php';
+                require 'src/query/query.php';
+                if (isset($_POST['query'])){
+                    $query = $_POST['query'];
+                    $results = mysqli_query($connection, search($query));
+                    foreach ($results as $info){  
+            ?>            
+            <tr>
+                <td>    <?php echo "Nome: " . $info['name'] . "<br>"; ?> </td>
+                        <?php echo "Descrição: " . $info['description'] . "<br>"; ?>
+                        <?php echo "Departamento: " . $info['department'] . "<br><br>"; ?>
+                        
+                        
+            <?php   }
+                } else {
+                    $results = mysqli_query($connection, $select_all);
+                    foreach ($results as $info){ ?>
+            
+            
+                        <?php echo "Nome: " . $info['name'] . "<br>"; ?>
+                        <?php echo "Descrição: " . $info['description'] . "<br>"; ?>
+                        <?php echo "Departamento: " . $info['department'] . "<br><br>"; ?>
+            
+            
+            <?php        }
+                }
+            ?>
+        </tbody>
+    </table>
 </div>
